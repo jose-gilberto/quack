8d84dbb2735295787f7d3cdaadb61a7892265ef8
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2026-07-06 22:07:41 -0300

    feat: add HDx and ReadMe

--- src/quack/quantifiers/__init__.py
+++ src/quack/quantifiers/__init__.py
@@ -1,6 +1,7 @@
 from quack.quantifiers._baselines import CC, PCC, ACC, PACC
 from quack.quantifiers._threshold import MedianSweep, X, T50, Max
-from quack.quantifiers._dmm import FormanMM, GAC
+from quack.quantifiers._dmm import FormanMM, GAC, GPAC
 
 
-__all__ = ["CC", "PCC", "ACC", "PACC", "MedianSweep", "X", "T50", "Max",]
+__all__ = ["CC", "PCC", "ACC", "PACC", "MedianSweep", "X", "T50", "Max",
+           "FormanMM", "GAC", "GPAC",]
