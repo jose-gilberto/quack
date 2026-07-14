721861a3e921892eadebacba2ea3e39cbb268526
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2026-07-14 10:40:27 -0300

    feat: add missing quantifiers on module init

--- src/quack/quantifiers/__init__.py
+++ src/quack/quantifiers/__init__.py
@@ -1,5 +1,6 @@
 from quack.quantifiers._baselines import CC, PCC, ACC, PACC
 from quack.quantifiers._threshold import MedianSweep, X, T50, Max
+from quack.quantifiers._dmm import HDy, FM, FormanMM, DyS, GAC, GPAC
 from quack.quantifiers._features import HDx, ReadMe
 from quack.quantifiers._iterators import EM, CDE
 from quack.quantifiers._probabilities import ED 
@@ -7,6 +8,7 @@ from quack.quantifiers._probabilities import ED
 
 __all__ = ["CC", "PCC", "ACC", "PACC",
            "MedianSweep", "X", "T50", "Max",
+           "HDy", "FM", "FormanMM", "DyS", "GAC", "GPAC",
            "ReadMe", "HDx",
            "EM", "CDE",
            "ED"]
