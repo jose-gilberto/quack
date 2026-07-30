41e3d85a679497d6dd674499744eccd4a67da5d3
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2026-07-30 19:55:10 -0300

    feat(coverage_plot): add coverage plot for bag prevalences

--- src/quack/visualization/__init__.py
+++ src/quack/visualization/__init__.py
@@ -2,6 +2,7 @@ from quack.visualization._colors import COLORBLIND_PALETTE, get_color_palette, g
 from quack.visualization._prevalence import prevalence_plot
 from quack.visualization._bias import bias_plot
 from quack.visualization._distribution import class_distribution_plot
+from quack.visualization._coverage import prevalence_coverage_plot
 
 __all__ = [
   'prevalence_plot',
@@ -10,4 +11,5 @@ __all__ = [
   'get_color_palette',
   'get_marker_cycle',
   'COLORBLIND_PALETTE',
+  'prevalence_coverage_plot',
 ]
